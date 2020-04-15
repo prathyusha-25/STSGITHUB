a9e46fb3b4f61eb170d6956d8ab1d799b5762dc2
prath <prath@PrathyushaG.attlocal.net>
2020-04-15 11:53:37 -0500

    Just created main.java file in com.example.demo

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Main {
+
+public void example() {
+	
+	System.out.println("Hello");
+}
+}
---
+++ src/main/java/com/example/demo/StsgithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsgithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsgithubApplication.class, args);
+	}
+
+}
